87594bba5e4fd06971c586841b6e440158702713
Rukaiya Khondaker <rnk23@ic.ac.uk>
2025-08-05 22:24:50 +0100

    Initial customisations

--- src/customisation/aboutme.ts
+++ src/customisation/aboutme.ts
@@ -3,13 +3,13 @@
  */
 
 // The main greeting
-export const hello = "Hello. I'm"
+export const hello = "Hi, I'm"
 
 // The name to be displayed with an underline
 export const name = "Rukaiya K"
 
 // The main description
-export const greeting = "But you can call me Kai :)"
+export const greeting = "but you can call me kai :)"
 
 // Whether to show the wave animation
 export const showWave = true
